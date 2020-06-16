39b99e1caf45b84b8948a62f5eae927418500792
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-06-16 17:56:03 +0100

    improved ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Fab } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import Grid from '@material-ui/core/Grid';
 
 
 class MainComp extends Component {
@@ -9,7 +10,7 @@ class MainComp extends Component {
     temp: 5,
     weather: "Sunny"
   }
-   fabstyle = {
+  fabstyle = {
     margin: 0,
     top: 'auto',
     right: 20,
@@ -22,11 +23,40 @@ class MainComp extends Component {
       <div className="mainImg">
         <h3 className="weather">{this.state.weather}</h3>
         <h1 className="temp">{this.state.temp}<span className="deg">o</span> </h1>
-
       </div>
       <Fab style={this.fabstyle} color="primary" size="large" aria-label="add">
         <AddIcon />
       </Fab>
+      <div>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <h1>hii</h1>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={1}>
+          <Grid item xs={2}>
+            30
+          <AddIcon color="primary" />
+          </Grid>
+          <Grid item xs={2}>
+            20
+          <AddIcon color="primary" />
+          </Grid>
+          <Grid item xs={2}>
+            10
+          <AddIcon color="primary" />
+          </Grid>
+          <Grid item xs={2}>
+            5
+          <AddIcon color="primary" />
+          </Grid>
+          <Grid item xs={2}>
+            55
+          <AddIcon color="primary" />
+          </Grid>
+        </Grid>
+      </div>
     </div>);
   }
 }
